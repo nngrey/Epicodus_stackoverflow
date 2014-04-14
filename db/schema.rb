d65f5c5e1915b242747015528eb870bba919d22f
Nathan Grey & Corey Norris <student@epicodus.com>
2014-04-14 09:46:18 -0700

    add answers table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140414164005) do
+ActiveRecord::Schema.define(version: 20140414164500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: true do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "questions", force: true do |t|
     t.string   "title"
