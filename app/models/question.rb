f8d0219cda2fd3cbe4f1347bdee957ff6a4a96c6
Nathan Grey & Corey Norris <student@epicodus.com>
2014-04-14 10:02:08 -0700

    pass validation and relationship specs

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,5 @@
 class Question < ActiveRecord::Base
+  validates :title, :presence => true
+  validates :content, :presence => true
+  has_many :answers
 end
---
+++ spec/models/answer_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+describe Answer do
+  it { should validate_presence_of :content }
+  it { should belong_to :question }
+end
